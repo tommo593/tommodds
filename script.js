9325f38224bd14809093e7ed33ef82efc866291a
Tommo <113673885+tommo593@users.noreply.github.com>
2023-02-16 14:06:40 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*let form = document.querySelector(".form");*/
+
+function revealCV() {
+    document.getElementById("cv").style.display = "block";
+    document.getElementById("tryagain").style.display = "none";
+  }
+
+  function tryAgain() {
+    document.getElementById("tryagain").style.display = "block";
+    document.getElementById("cv").style.display = "none";
+  }
+
